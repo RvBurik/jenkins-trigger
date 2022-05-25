1d267351eba2d97875d639b77e6781568581ff6a
Ricardo van Burik <ricardovanburik@hotmail.com>
2022-05-25 13:58:54 +0200

    Test

---
+++ index.js
@@ -0,0 +1,110 @@
+const request = require('request');
+const core = require('@actions/core');
+
+// create auth token for Jenkins API
+const API_TOKEN = Buffer.from(`${core.getInput('user_name')}:${core.getInput('api_token')}`).toString('base64');
+
+let timer = setTimeout(() => {
+  core.setFailed("Job Timeout");
+  core.error("Exception Error: Timed out");
+  }, (Number(core.getInput('timeout')) * 1000));
+
+const sleep = (seconds) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, (seconds * 1000));
+  });
+};
+
+async function requestJenkinsJob(jobName, params) {
+  const jenkinsEndpoint = core.getInput('url');
+  core.info(`>>> Meegegeven URL: ${jenkinsEndpoint}`);
+  core.info(`>>> Url die meegegeven wordt aan het request: ${jenkinsEndpoint}/job/${jobName}/buildWithParameters`)
+  core.info(`>>> API TOKEN: ${API_TOKEN}`)
+  const req = {
+    method: 'POST',
+    url: `${jenkinsEndpoint}/job/${jobName}/buildWithParameters`,
+    form: params,
+    headers: {
+      'Authorization': `Basic ${API_TOKEN}`
+    }
+  }
+  await new Promise((resolve, reject) => request(req)
+    .on('response', (res) => {
+      core.info(`>>> Job is started! YOLO`);
+      core.info(req);
+      resolve();
+    })
+    .on("error", (err) => {
+      core.setFailed(err);
+      core.error(JSON.stringify(err));
+      clearTimeout(timer);
+      reject();
+    })
+  );
+}
+
+async function getJobStatus(jobName) {
+  const jenkinsEndpoint = core.getInput('url');
+  const req = {
+    method: 'get',
+    url: `${jenkinsEndpoint}/job/${jobName}/lastBuild/api/json`,
+    headers: {
+      'Authorization': `Basic ${API_TOKEN}`
+    }
+  }
+  return new Promise((resolve, reject) =>
+      request(req, (err, res, body) => {
+        if (err) {
+          clearTimeout(timer);
+          reject(err);
+        }
+        resolve(JSON.parse(body));
+      })
+    );
+}
+async function waitJenkinsJob(jobName, timestamp) {
+  core.info(`>>> Waiting for "${jobName}" ...`);
+  core.info(`>>> TEST TEST TEST `);
+  while (true) {
+    let data = await getJobStatus(jobName);
+    if (data.timestamp < timestamp) {
+      core.info(`>>> Job is not started yet... Wait 5 seconds more...`)
+    } else if (data.result == "SUCCESS") {
+      core.info(`>>> Job "${data.fullDisplayName}" successfully completed!`);
+      break;
+    } else if (data.result == "FAILURE" || data.result == "ABORTED") {
+      throw new Error(`Failed job ${data.fullDisplayName}`);
+    } else {
+      core.info(`>>> Current Duration: ${data.duration}. Expected: ${data.estimatedDuration}`);
+    }
+    await sleep(5); // API call interval
+  }
+}
+
+async function main() {
+  try {
+    let params = {};
+    let startTs = + new Date();
+    let jobName = core.getInput('job_name');
+    core.info(`Dit is de Jobname: ${jobName}`);
+    if (core.getInput('parameter')) {
+      params = JSON.parse(core.getInput('parameter'));
+      core.info(`>>> Parameter ${params.toString()}`);
+    }
+    // POST API call
+    await requestJenkinsJob(jobName, params);
+
+    // Waiting for job completion
+    if (core.getInput('wait') == 'true') {
+      await waitJenkinsJob(jobName, startTs);
+    }
+  } catch (err) {
+    core.setFailed(err.message);
+    core.error(err.message);
+  } finally {
+    clearTimeout(timer);
+  }
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED="0";
+main();
